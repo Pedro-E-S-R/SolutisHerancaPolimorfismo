fbca48fc5110218db8c3ca9960361c77278c1f00
Pedro Rocha <pedr012012@outlook.com>
2024-08-25 03:01:04 -0300

    Correção questão 11 25/08

--- src/br/com/solutis/exercicio/questao11/TesteToString.java
+++ src/br/com/solutis/exercicio/questao11/TesteToString.java
@@ -5,17 +5,36 @@ import br.com.solutis.exercicio.questao8.Cargo;
 
 public class TesteToString {
     public static void main(String[] args) {
-        Funcionario[] f = new Funcionario[3];
-        f[0] = new FuncionarioEnsinoBasico("Marcos",125, Cargo.VENDEDOR,
-                "Escola Basica");
-        f[1] = new FuncionarioEscolaMedia("José",125, Cargo.GERENTE,
-                "Escola Basica","Escola Media");
-        f[2] = new FuncionarioGraduado("José",125, Cargo.SUPERVISOR,
-                "Escola Basica","Escola Media","Univercidade Graduada");
+        Funcionario[] f = new Funcionario[10];
+        f[0] = new FuncionarioEnsinoBasico("Rafael",
+                111, Cargo.GERENTE,"Dom Carlos");
+        f[1] = new FuncionarioEnsinoBasico("Joel",
+                112,Cargo.SUPERVISOR,"Dom Carlos");
+        f[2] = new FuncionarioEnsinoBasico("Marcos",
+                113,Cargo.SUPERVISOR,"Dom Carlos");
+        f[3] = new FuncionarioEnsinoBasico("Maria",
+                114,Cargo.VENDEDOR,"Dom Carlos");
+        f[4] =  new FuncionarioEscolaMedia("Nasha",
+                115,Cargo.VENDEDOR,"Dom Carlos","José Alfredo Neto");
+        f[5] =  new FuncionarioEscolaMedia("Ronivaldo",
+                116,Cargo.VENDEDOR,"Dom Carlos","José Alfredo Neto");
+        f[6] =  new FuncionarioEscolaMedia("Cleidesvaldo",
+                117,Cargo.VENDEDOR,"Dom Carlos","José Alfredo Neto");
+        f[7] =  new FuncionarioEscolaMedia("Valdemiro",
+                118,Cargo.VENDEDOR,"Dom Carlos","José Alfredo Neto");
+        f[8] = new FuncionarioGraduado("Lydyynnsi",
+                119,Cargo.VENDEDOR,"Dom Carlos","José Alfredo Neto",
+                "Universidade graduada");
+        f[9] = new FuncionarioGraduado("Joao",
+                120,Cargo.VENDEDOR,"Dom Carlos","José Alfredo Neto",
+                "Universidade graduada");
 
+        double gastoSalario =0;
         for(Funcionario funcionario : f) {
+            gastoSalario += funcionario.getRendaTotal();
             System.out.println(funcionario);
-            System.out.println("--------------------------");
+            System.out.println("----------------------");
         }
+        System.out.println("Gasto total da empresa é de R$"+ gastoSalario);
     }
 }
